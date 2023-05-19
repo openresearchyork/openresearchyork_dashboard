3e09a3f7a9b0fe5424f9c017f141d7d30fa567d4
Paula Schirrmacher <schirrmacher@posteo.de>
2023-05-19 11:19:48 +0100

    added bronze access explicitly, changed order of stacked bars

--- app.R
+++ app.R
@@ -24,13 +24,11 @@ OA <- OA[1:(nrow(OA)-2),]#update dataframe to delete last rows containing metada
 
 OA$Year<-as.factor(OA$Year)#column 'year' should be a factor ('category') for grouping data
 
-OA$Open.Access2<-as.factor(OA$Open.Access)#column 'open access's should be a factor
+OA$Open.Access2<-as.factor(OA$Open.Access)#create new column based on 'open access' (should be a factor)
 
-levels(OA$Open.Access2)<-c("Not Open Access", "Not Open Access", "Green", "Gold", "Gold", "Green", "Hybrid Gold", "Hybrid Gold")# rename some of the levels
+levels(OA$Open.Access2)<-c("Not Open Access", "Bronze", "Green", "Gold", "Gold", "Green", "Hybrid Gold", "Hybrid Gold")# rename some of the levels
 
-OA$generalOA<-ifelse(OA$Open.Access2=="Not Open Access", "Closed Access", "Open Access")#create a new variable that groups into 'closed' and 'open access'
-
-OA$n<-rep(1,nrow(OA))#create new variable for summary stats
+OA$generalOA<-ifelse(OA$Open.Access2=="Not Open Access" | OA$Open.Access2=="Bronze", "Closed Access", "Open Access")#create a new variable that groups into 'closed' and 'open access'
 
 OA1<-OA %>%
   #create summary stats by year and OA format
@@ -42,7 +40,7 @@ OA1<-OA %>%
 
 version<-read.csv("Publications_at_the_University_of_York_SciVal.csv", header=FALSE, skip=9, nrows=1)#retrieve metadata
 
-info_text<-paste("Data retrieved from Unpawall.com via SciVal. All publications affiliated with the University of York indexed on Scopus are included, data last updated ", version[,2], ". Bronze access was classed as closed access.", sep="")#create info text to be displayed in app
+info_text<-paste("Data retrieved from Unpawall.com via SciVal. All publications affiliated with the University of York indexed on Scopus are included, data last updated ", version[,2], ".", sep="")#create info text to be displayed in app
 
 
 #### USER INTERFACE ####
@@ -61,8 +59,8 @@ ui <- fluidPage(
       awesomeCheckboxGroup(
         inputId = "access",
         label = "Choose Open Access Format", 
-        choices = levels(OA$Open.Access2),
-        selected = levels(OA$Open.Access2)),
+        choices = levels(OA1$Open.Access2),
+        selected = levels(OA1$Open.Access2)),
       sliderInput(
         inputId = "year", 
         label = "Select Year", 
@@ -103,9 +101,10 @@ server <- function(input, output, session){
   output$plot_OA<-plotly::renderPlotly({
     # Plot selected year and access
     rval_OAfiltered() %>% 
+      mutate(Open.Access2 = factor(Open.Access2, levels = c("Hybrid Gold", "Gold", "Green", "Bronze", "Not Open Access")))%>%
       ggplot(aes(x = generalOA, y = prop, fill=Open.Access2)) +
       geom_col(color="black", size=0.1)+
-      scale_fill_manual(values=c("gray50", "seagreen", "goldenrod", "darkorange3"))+
+      scale_fill_manual(values=c("khaki3","goldenrod", "palegreen4", "coral3", "gray50"))+
       scale_y_continuous(labels = scales::percent, limits=c(0,0.8))+
       labs(x="", y="Publications in selected year [%]", fill="Open Access Format")+
       theme_classic(base_size=12)
